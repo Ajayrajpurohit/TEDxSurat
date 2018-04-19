30a2d87847cf458543820f475699bc127df10776
Rishipal Yadav <yadav.rishipal001@gmail.com>
2018-04-19 11:57:02 +0530

    Create Index.js

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  console.log('window loaded');
+  document.querySelector('#posterbtn').click();
+  // document.querySelector('#posterbtn').addEventListener('click', function() {
+  //   console.log('clicked poster btn');
+  // });
+};
